5b050552a5a063b459f55938901f6fd5a2b524f8
CFV <vescovocarlofederico@gmail.com>
2018-07-25 20:27:33 +0200

    Travis 1

--- GUI/test.py
+++ GUI/test.py
@@ -1,7 +1,14 @@
+import signal
 import tornado.ioloop
 import tornado.web
 import os
 
+TIMEOUT = 5
+
+def interrupted(signum, frame):
+    print("TRAVIS")
+    exit(0)
+signal.signal(signal.SIGALRM, interrupted)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -20,7 +27,14 @@ class Application(tornado.web.Application):
         }
         tornado.web.Application.__init__(self, handlers, **settings)
 
+print("OK")
+print("Build succeded!")
+signal.alarm(TIMEOUT)
+travis = int(input("Write 1: "))
+signal.alarm(0)
 
 http_server = tornado.httpserver.HTTPServer(Application())
 http_server.listen(8080)
 tornado.ioloop.IOLoop.instance().start()
+
+
